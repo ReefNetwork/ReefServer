35038a31cf601526b68f48c21918c28840441ae8
Ree_ <ree.dev.jp@gmail.com>
2020-04-13 14:50:54 +0900

    [change] 非同期がbot側がやってくれるようにしたから変更

--- src/main/kotlin/net/ree_jp/reefseichi/event/DiscordSendListener.kt
+++ src/main/kotlin/net/ree_jp/reefseichi/event/DiscordSendListener.kt
@@ -4,30 +4,34 @@ import cn.nukkit.event.EventHandler
 import cn.nukkit.event.Listener
 import cn.nukkit.event.player.PlayerChatEvent
 import cn.nukkit.event.player.PlayerJoinEvent
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import cn.nukkit.event.player.PlayerQuitEvent
 import net.ree_jp.reefseichi.system.discord.ReefDiscord
 
 class DiscordSendListener : Listener {
 
     @EventHandler
-    fun onChat(ev: PlayerChatEvent) {
+    fun joinForSendDiscord(ev: PlayerJoinEvent) {
+        val p = ev.player
+        val n = p.name
+
+        ReefDiscord.getBot().sendMessage("$n さんがログインしました")
+    }
+
+    @EventHandler
+    fun quitForSendDiscord(ev: PlayerQuitEvent) {
+        val p = ev.player
+        val n = p.name
+        val reason = ev.reason
+
+        ReefDiscord.getBot().sendMessage("$n さんが$reason でログアウトしました")
+    }
+
+    @EventHandler
+    fun chatForSendDiscord(ev: PlayerChatEvent) {
         val p = ev.player
         val n = p.name
         val message = ev.message
 
-        GlobalScope.launch {
-            ReefDiscord.getBot().sendMessage("[$n] `$message`")
-        }
-    }
-
-    @EventHandler
-    fun onJoin(ev: PlayerJoinEvent) {
-        val p = ev.player
-        val n = p.name
-
-        GlobalScope.launch {
-            ReefDiscord.getBot().sendMessage("$n さんがやってきました")
-        }
+        ReefDiscord.getBot().sendMessage("[$n] `$message`")
     }
 }
