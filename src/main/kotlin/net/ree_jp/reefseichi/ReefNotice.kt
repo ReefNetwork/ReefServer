3e59ca4d8b8dc18e178451c6619716d4f594c54c
Ree_ <ree.dev.jp@gmail.com>
2020-04-27 23:42:58 +0900

    add: ReefNotice.kt

---
+++ src/main/kotlin/net/ree_jp/reefseichi/ReefNotice.kt
@@ -0,0 +1,23 @@
+/*
+ * RRRRRR                         jjj
+ * RR   RR   eee    eee               pp pp
+ * RRRRRR  ee   e ee   e _____    jjj ppp  pp
+ * RR  RR  eeeee  eeeee           jjj pppppp
+ * RR   RR  eeeee  eeeee          jjj pp
+ *                              jjjj  pp
+ *
+ * Copyright (c) 2020. Ree-jp.  All Rights Reserved.
+ */
+
+package net.ree_jp.reefseichi
+
+import cn.nukkit.utils.TextFormat
+
+class ReefNotice {
+
+    companion object {
+        val SUCCESS = "${TextFormat.GREEN}>> ${TextFormat.RESET} "
+        val PROCESSING = "${TextFormat.GREEN}>> ${TextFormat.RESET} "
+        val ERROR = "${TextFormat.GREEN}>> ${TextFormat.RESET} "
+    }
+}
