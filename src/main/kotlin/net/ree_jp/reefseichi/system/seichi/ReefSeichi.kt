37398aaa9405f7bd1592afaa45a6a197f0e4cdcb
Ree_ <ree.dev.jp@gmail.com>
2020-04-30 21:15:41 +0900

    update: update(いろいろ)

--- src/main/kotlin/net/ree_jp/reefseichi/system/seichi/ReefSeichi.kt
+++ src/main/kotlin/net/ree_jp/reefseichi/system/seichi/ReefSeichi.kt
@@ -12,18 +12,58 @@
 package net.ree_jp.reefseichi.system.seichi
 
 import net.ree_jp.reefseichi.ReefSeichiPlugin
+import net.ree_jp.reefseichi.system.seichi.api.SeichiAPI
+import net.ree_jp.reefseichi.system.seichi.data.SeichiData
+import net.ree_jp.reefseichi.system.seichi.data.Skill
 import net.ree_jp.reefseichi.system.seichi.sqlite.SeichiHelper
 
 class ReefSeichi {
 
-    companion object{
-        private lateinit var helper: SeichiHelper
+    companion object {
 
-        fun getHelper(): SeichiHelper{
-            if (!::helper.isInitialized) {
-                helper = SeichiHelper(ReefSeichiPlugin.getInstance().dataFolder.path)
+        private lateinit var instance: ReefSeichi
+
+        fun getInstance(): ReefSeichi {
+            if (!::instance.isInitialized) {
+                instance = ReefSeichi()
             }
-            return helper
+            return instance
         }
+    }
+
+    private lateinit var api: SeichiAPI
+
+    private lateinit var helper: SeichiHelper
+
+    fun getAPI(): SeichiAPI {
+        if (!::api.isInitialized) {
+            api = SeichiAPI(getHelper())
+        }
+        return api
+    }
+
+    fun getHelper(): SeichiHelper {
+        if (!::helper.isInitialized) {
+            helper = SeichiHelper(ReefSeichiPlugin.getInstance().dataFolder.path)
+        }
+        return helper
+    }
+
+    fun getLevel(xp: Int): Int {
+        TODO()
+    }
+
+    fun getDefault(xuid: String): SeichiData {
+        return SeichiData(
+            xuid,
+            getDefaultSkill(),
+            listOf(getDefaultSkill()),
+            0,
+            0
+        )
+    }
+
+    private fun getDefaultSkill(): Skill {
+        return Skill("default", 0, 1, 1, 1, 1)
     }
 }
