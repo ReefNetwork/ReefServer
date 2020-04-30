6ae5bbbae289b515607ccc4be4c40189f83d630a
Ree_ <ree.dev.jp@gmail.com>
2020-04-30 21:21:19 +0900

    add: SeichiAPI.kt

---
+++ src/main/kotlin/net/ree_jp/reefseichi/system/seichi/api/SeichiAPI.kt
@@ -0,0 +1,41 @@
+/*
+ * RRRRRR                         jjj
+ * RR   RR   eee    eee               pp pp
+ * RRRRRR  ee   e ee   e _____    jjj ppp  pp
+ * RR  RR  eeeee  eeeee           jjj pppppp
+ * RR   RR  eeeee  eeeee          jjj pp
+ *                              jjjj  pp
+ *
+ * Copyright (c) 2020. Ree-jp.  All Rights Reserved.
+ */
+
+package net.ree_jp.reefseichi.system.seichi.api
+
+import net.ree_jp.reefseichi.system.seichi.sqlite.SeichiHelper
+
+class SeichiAPI(private val helper: SeichiHelper) {
+
+    fun addXp(xuid: String, xp: Int) {
+        setXp(xuid, getXp(xuid) + xp)
+    }
+
+    fun removeXp(xuid: String, xp: Int) {
+        val rewrite = getXp(xuid) - xp
+        if (rewrite < 0) throw Exception("xp minus")
+        setXp(xuid, rewrite)
+    }
+
+    fun getXp(xuid: String): Int {
+        if (!helper.isExists(xuid)) throw Exception("player data not found")
+
+        return helper.getData(xuid).xp
+    }
+
+    private fun setXp(xuid: String, xp: Int) {
+        if (!helper.isExists(xuid)) throw Exception("player data not found")
+
+        val data = helper.getData(xuid)
+        data.xp = xp
+        helper.setData(xuid, data)
+    }
+}
