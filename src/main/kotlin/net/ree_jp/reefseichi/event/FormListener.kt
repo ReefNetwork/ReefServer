82e555e25a2446237bff98d345ef9f07cfdfd256
Ree_ <ree.dev.jp@gmail.com>
2020-05-10 19:47:06 +0900

    update: 参加した時時計を持っていなければ渡すように - シーランタンを叩けばmenuが開くように

--- src/main/kotlin/net/ree_jp/reefseichi/event/FormListener.kt
+++ src/main/kotlin/net/ree_jp/reefseichi/event/FormListener.kt
@@ -12,11 +12,14 @@
 package net.ree_jp.reefseichi.event
 
 import cn.nukkit.Server
+import cn.nukkit.block.Block
 import cn.nukkit.event.EventHandler
 import cn.nukkit.event.Listener
 import cn.nukkit.event.player.PlayerFormRespondedEvent
 import cn.nukkit.event.player.PlayerInteractEvent
+import cn.nukkit.event.player.PlayerJoinEvent
 import cn.nukkit.item.Item
+import cn.nukkit.utils.TextFormat
 import net.ree_jp.reefseichi.ReefNotice
 import net.ree_jp.reefseichi.ReefSeichiPlugin
 import net.ree_jp.reefseichi.form.MainMenuForm
@@ -27,18 +30,32 @@ class FormListener : Listener {
     private val formCool = mutableListOf<String>()
 
     @EventHandler
+    fun joinForGiveMenuItem(ev: PlayerJoinEvent) {
+        val p = ev.player
+        val inv = p.inventory
+        val menu = Item.get(Item.CLOCK).setCustomName("${TextFormat.GRAY}Menu")
+
+        if (!inv.contains(menu)) {
+            inv.addItem(menu)
+            p.sendMessage("${ReefNotice.SUCCESS}Menuは時計を地面にクリックするかシーランタンをクリックするまたは/menuと打つと開きます")
+            p.sendMessage("${ReefNotice.SUCCESS}もし時計を無くしてしまったときはもう一度ログインすると貰えます")
+        }
+    }
+
+    @EventHandler
     fun touchForOpenMenu(ev: PlayerInteractEvent) {
         val p = ev.player
         val xuid = p.loginChainData.xuid
 
         try {
-            if (ev.item.id == Item.CLOCK && !formCool.contains(xuid)) {
-                p.showFormWindow(MainMenuForm(p, ""))
+            if (((ev.item.id == Item.CLOCK) || (ev.block.id == Block.SEA_LANTERN)) && !formCool.contains(xuid)) {
+                formCool.add(xuid)
                 Server.getInstance().scheduler.scheduleDelayedTask(
                     ReefSeichiPlugin.getInstance(),
                     { formCool.remove(xuid) },
                     10, false
                 )
+                p.showFormWindow(MainMenuForm(p, ""))
             }
         } catch (ex: Exception) {
             ex.printStackTrace()
