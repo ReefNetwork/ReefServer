502da0f1daacaa0fe55bfa2c5da7798ad45bc2a4
Ree_ <ree.dev.jp@gmail.com>
2020-05-13 17:06:18 +0900

    add: ReefFly update: add fly mywarp

--- src/main/kotlin/net/ree_jp/reefseichi/form/MainMenuForm.kt
+++ src/main/kotlin/net/ree_jp/reefseichi/form/MainMenuForm.kt
@@ -16,16 +16,31 @@ import cn.nukkit.form.element.ElementButton
 import cn.nukkit.form.response.FormResponse
 import cn.nukkit.form.response.FormResponseSimple
 import cn.nukkit.form.window.FormWindowSimple
+import cn.nukkit.utils.TextFormat
+import net.bbo51dog.ecokkit.api.EcokkitAPI
+import net.ree_jp.reefseichi.ReefNotice
+import net.ree_jp.reefseichi.system.fly.ReefFly
+import net.ree_jp.reefseichi.system.mywarp.form.MyWarpSelectForm
 import net.ree_jp.reefseichi.system.seichi.form.SkillSelectForm
 import net.ree_jp.storage.StackStoragePlugin
 
 class MainMenuForm(player: Player, content: String) : Response, FormWindowSimple("ReefSeichiMenu", content) {
 
     init {
+        val xuid = player.loginChainData.xuid
+        val fly = ReefFly.getInstance().getApi()
+        val ecokkit = EcokkitAPI.instance
+
         addButton(ElementButton("ワールドを移動する") { player.showFormWindow(MainMenuForm(player, "")) })
         addButton(ElementButton("スキルを選択する") { player.showFormWindow(SkillSelectForm(player, "")) })
-        addButton(ElementButton("フライを") { player.showFormWindow(MainMenuForm(player, "")) })
+        addButton(ElementButton("フライを${if (fly.isFly(xuid)) "${TextFormat.GREEN}有効" else "${TextFormat.GRAY}無効"}${TextFormat.RESET}にする\n${ReefFly.FLY_PRICE}/m coin") {
+            if (fly.isCanFlyWorld(player.getLevel()) && ecokkit.getMoney(xuid) >= ReefFly.FLY_PRICE) {
+                fly.setFly(player)
+                player.sendMessage("フライを${if (fly.isFly(xuid)) "${TextFormat.GREEN}有効" else "${TextFormat.GRAY}無効"}${TextFormat.RESET}にしました")
+            } else player.sendMessage("${ReefNotice.SUCCESS}このワールドではフライは使えないかお金が足りません")
+        })
         addButton(ElementButton("ストレージを開く") { StackStoragePlugin.getInstance().getApi().sendGui(player) })
+        addButton(ElementButton("マイワープ") { player.showFormWindow(MyWarpSelectForm(player, "")) })
     }
 
     override fun handleResponse(player: Player, response: FormResponse) {
