df1b60f0c813368ac0ae7954700dd4668f8345bd
Ree_ <mail@ree-jp.net>
2020-07-26 15:30:02 +0900

    update: run

--- run.cmd
+++ run.cmd
@@ -2,7 +2,7 @@ move build\libs\ReefSeichi-1.0-SNAPSHOT-all.jar D:\docker\nukkit\plugins
 cd D:\docker\nukkit\plugins
 @echo StartingServer...
 docker-compose up -d
-docker attach nukkit_coral_reef_1
+docker attach reef
 @echo Initializing...
 docker-compose down
 @cd D:\code\nukkitPlugin\ReefSeichi
